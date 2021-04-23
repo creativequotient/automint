a0073ebd51bff9eb792f5e3ebdc41f8699530436
kangtinglee <marcus_lee@u.nus.edu>
2021-04-24 01:51:18 +0800

    Update setup.py with dependency

--- setup.py
+++ setup.py
@@ -1,12 +1,17 @@
 import setuptools
 
 setuptools.setup(
-    name="automint", # Replace with your own username
+    name="automint",
     version="0.0.1",
-    description="A small example package",
+    author="Marcus Lee",
+    author_email="mlkt12x@gmail.com",
+    description="Cardano NFT minting and transactions python library",
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
     python_requires=">=3.7",
+    install_requires = [
+        'requests'
+    ]
 )
