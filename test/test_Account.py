82f7c7d406aeed95511a5e03fbc4baf0ee73f5a9
kangtinglee <marcus_lee@u.nus.edu>
2021-04-27 13:28:46 +0800

    Add more unit tests

--- test/test_Account.py
+++ test/test_Account.py
@@ -5,12 +5,24 @@ from automint.account import Account
 
 class AccountTests(unittest.TestCase):
     def test_lovelace_addition(self):
-        """
-        Test that it can sum a list of integers
-        """
         account = Account().add_lovelace(1000)
         self.assertEqual(account.get_lovelace(), 1000)
         self.assertEqual(account.get_ada(), 0.001)
+
+    def test_account_addition(self):
+        account_a = Account().add_lovelace(1500000)
+        account_b = Account().add_lovelace(1500000)
+        account_c = account_a + account_b
+        self.assertEqual(account_c.get_lovelace(), 3000000)
+        self.assertEqual(account_c.get_ada(), 3)
+
+    def test_account_str(self):
+        account = Account().add_lovelace(1500000)
+        self.assertEqual(str(account), '1500000')
+
+        account = Account().add_lovelace(1500000).add_native_token('12345.tokenA', 2)
+        self.assertEqual(str(account), '1500000+"2 12345.tokenA"')
+
 
 if __name__ == '__main__':
     unittest.main()
